a14166c66a17fed101a45737fa23a9aec6f3f6e9
seva931 <seva.dmitro96@mail.ru>
2025-09-25 19:02:42 +0300

    fix

--- src/functional_interface/MainMath.java
+++ src/functional_interface/MainMath.java
@@ -6,11 +6,11 @@ public class MainMath {
         MathOperation add = (a, b) -> a + b;
         System.out.println("Сложение: " + add.operate(10,5));
         MathOperation subtraction = (a, b) -> a - b;
-        System.out.println("Вычитание: " + add.operate(210,55));
+        System.out.println("Вычитание: " + subtraction.operate(210,55));
         MathOperation division = (a, b) -> a / b;
-        System.out.println("Деление: " + add.operate(1000, 10));
+        System.out.println("Деление: " + division.operate(1000, 10));
         MathOperation multiplication = (a, b) -> a * b;
-        System.out.println("Умножение: " + add.operate(10,10));
+        System.out.println("Умножение: " + multiplication.operate(10,10));
 
         System.out.println("Задача 2");
         Runnable print = new Runnable() {
